f20398b13bb6bac7435904b28a52af6d34a72f9f
Jannik Wempe <jannik@wempe.dev>
2025-02-21 08:47:56 +0100

    updates

--- infra/www.ts
+++ infra/www.ts
@@ -1,35 +1,11 @@
-// basically reflects the default SST cache policy
-// but excludes 'p' from query string caching
-
 import { hostname } from './dns';
-
-// since that is changing for server islands and causes cache misses
-const cachePolicy = new aws.cloudfront.CachePolicy('AstroServerCachePolicy', {
-	comment: 'Custom cache policy for Astro server',
-	minTtl: 0,
-	maxTtl: 31536000,
-	defaultTtl: 0,
-	parametersInCacheKeyAndForwardedToOrigin: {
-		enableAcceptEncodingBrotli: true,
-		enableAcceptEncodingGzip: true,
-		cookiesConfig: {
-			cookieBehavior: 'none',
-		},
-		headersConfig: {
-			headerBehavior: 'none',
-		},
-		queryStringsConfig: {
-			queryStringBehavior: 'allExcept',
-			queryStrings: {
-				items: ['p'],
-			},
-		},
-	},
-});
 
 export const astro = new sst.aws.Astro('Astro', {
 	path: './apps/www',
-	cachePolicy: cachePolicy.id,
+	dev: {
+		autostart: true,
+		command: 'astro dev',
+	},
 	domain: {
 		name: hostname,
 		dns: sst.cloudflare.dns(),
