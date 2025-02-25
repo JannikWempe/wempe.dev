09d56a6bd98c7b81863715ad3c3b1d75163f8675
Jannik Wempe <jannik@wempe.dev>
2025-02-25 10:28:35 +0100

    improve image handling

--- apps/www/src/utils/image.ts
+++ apps/www/src/utils/image.ts
@@ -3,3 +3,6 @@ export type ImageSource = string | { src: string };
 export function getCoverImageSrc(cover: ImageSource): string {
 	return typeof cover === 'string' ? cover : cover.src;
 }
+
+export const isExternalCoverImage = (image: unknown): image is string =>
+	typeof image === 'string' && image.startsWith('http');
