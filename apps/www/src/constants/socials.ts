2b0f01ba1fbc399ab76c79c7946970381878556b
Jannik Wempe <jannik@wempe.dev>
2025-02-25 20:03:26 +0100

    remove hashnode from nav

--- apps/www/src/constants/socials.ts
+++ apps/www/src/constants/socials.ts
@@ -3,5 +3,4 @@ export const X_URL = `https://x.com/${TWITTER_HANDLE.replace('@', '')}`;
 export const BLUESKY_URL = 'https://bsky.app/profile/jannikwempe.com';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/jannik-wempe';
 export const GITHUB_URL = 'https://github.com/jannikwempe';
-export const HASHNODE_URL = 'https://hashnode.com/@jannikwempe';
 export const EMAIL = 'jannik@wempe.dev';
