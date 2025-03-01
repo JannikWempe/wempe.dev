bd37f1cbf681f133033d62f376b094d9dfbaa53d
Jannik Wempe <jannik@wempe.dev>
2025-03-01 21:22:34 +0100

    change bluesky url

--- apps/www/src/constants/socials.ts
+++ apps/www/src/constants/socials.ts
@@ -1,6 +1,6 @@
 export const TWITTER_HANDLE = '@JannikWempe';
 export const X_URL = `https://x.com/${TWITTER_HANDLE.replace('@', '')}`;
-export const BLUESKY_URL = 'https://bsky.app/profile/jannikwempe.com';
+export const BLUESKY_URL = 'https://bsky.app/profile/wempe.dev';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/jannik-wempe';
 export const GITHUB_URL = 'https://github.com/jannikwempe';
 export const EMAIL = 'jannik@wempe.dev';
