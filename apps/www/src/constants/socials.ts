a2d41603c0d65809d87fbeb7cdf729808acf8798
Jannik Wempe <jannik@wempe.dev>
2025-03-26 10:48:08 +0100

    add cv

--- apps/www/src/constants/socials.ts
+++ apps/www/src/constants/socials.ts
@@ -1,6 +1,10 @@
-export const TWITTER_HANDLE = '@JannikWempe';
-export const X_URL = `https://x.com/${TWITTER_HANDLE.replace('@', '')}`;
-export const BLUESKY_URL = 'https://bsky.app/profile/wempe.dev';
-export const LINKEDIN_URL = 'https://www.linkedin.com/in/jannik-wempe';
-export const GITHUB_URL = 'https://github.com/jannikwempe';
+export const X_HANDLE = '@JannikWempe';
+export const X_URL = `https://x.com/${X_HANDLE.replace('@', '')}`;
+export const BLUESKY_HANDLE = 'wempe.dev';
+export const BLUESKY_URL = `https://bsky.app/profile/${BLUESKY_HANDLE}`;
+export const LINKEDIN_HANDLE = 'jannik-wempe';
+export const LINKEDIN_URL = `https://www.linkedin.com/in/${LINKEDIN_HANDLE}`;
+export const GITHUB_HANDLE = 'jannikwempe';
+export const GITHUB_URL = `https://github.com/${GITHUB_HANDLE}`;
+
 export const EMAIL = 'jannik@wempe.dev';
