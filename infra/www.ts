1027f7e744845c27985c771c834896f404d4b874
Jannik Wempe <jannik@wempe.dev>
2025-12-30 09:14:30 +0100

    use node 22

--- infra/www.ts
+++ infra/www.ts
@@ -1,72 +1,77 @@
-import { hostname } from './dns';
+import { hostname } from "./dns";
 
 const plausibleOrigin = {
-	domainName: 'plausible.io',
-	originId: 'plausibleCustomOrigin',
-	customOriginConfig: {
-		httpPort: 80,
-		httpsPort: 443,
-		originSslProtocols: ['TLSv1.2'],
-		originProtocolPolicy: 'https-only',
-	},
+  domainName: "plausible.io",
+  originId: "plausibleCustomOrigin",
+  customOriginConfig: {
+    httpPort: 80,
+    httpsPort: 443,
+    originSslProtocols: ["TLSv1.2"],
+    originProtocolPolicy: "https-only",
+  },
 } satisfies aws.types.input.cloudfront.DistributionOrigin;
 
 const analyticsScriptBehavior = {
-	pathPattern: '/js/script.*',
-	targetOriginId: plausibleOrigin.originId,
-	viewerProtocolPolicy: 'https-only',
-	allowedMethods: ['GET', 'HEAD'],
-	cachedMethods: ['GET', 'HEAD'],
-	defaultTtl: 0,
-	maxTtl: 0,
-	compress: false,
-	forwardedValues: {
-		cookies: {
-			forward: 'none',
-		},
-		queryString: false,
-	},
+  pathPattern: "/js/script.*",
+  targetOriginId: plausibleOrigin.originId,
+  viewerProtocolPolicy: "https-only",
+  allowedMethods: ["GET", "HEAD"],
+  cachedMethods: ["GET", "HEAD"],
+  defaultTtl: 0,
+  maxTtl: 0,
+  compress: false,
+  forwardedValues: {
+    cookies: {
+      forward: "none",
+    },
+    queryString: false,
+  },
 } satisfies $util.UnwrappedArray<
-	$util.Input<aws.types.input.cloudfront.DistributionOrderedCacheBehavior>
+  $util.Input<aws.types.input.cloudfront.DistributionOrderedCacheBehavior>
 >[number];
 
 const eventApiBehavior = {
-	pathPattern: '/api/event',
-	targetOriginId: plausibleOrigin.originId,
-	viewerProtocolPolicy: 'https-only',
-	originRequestPolicyId: 'acba4595-bd28-49b8-b9fe-13317c0390fa',
-	// See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
-	cachePolicyId: '4135ea2d-6df8-44a3-9df3-4b5a84be39ad',
-	allowedMethods: ['GET', 'HEAD', 'OPTIONS', 'PUT', 'POST', 'PATCH', 'DELETE'],
-	cachedMethods: ['GET', 'HEAD'],
+  pathPattern: "/api/event",
+  targetOriginId: plausibleOrigin.originId,
+  viewerProtocolPolicy: "https-only",
+  originRequestPolicyId: "acba4595-bd28-49b8-b9fe-13317c0390fa",
+  // See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html
+  cachePolicyId: "4135ea2d-6df8-44a3-9df3-4b5a84be39ad",
+  allowedMethods: ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"],
+  cachedMethods: ["GET", "HEAD"],
 } satisfies $util.UnwrappedArray<
-	$util.Input<aws.types.input.cloudfront.DistributionOrderedCacheBehavior>
+  $util.Input<aws.types.input.cloudfront.DistributionOrderedCacheBehavior>
 >[number];
 
-export const astro = new sst.aws.Astro('Astro', {
-	path: 'apps/www',
-	dev: {
-		autostart: true,
-		command: 'astro dev',
-	},
-	domain: {
-		name: hostname,
-		dns: sst.cloudflare.dns(),
-		redirects: [`www.${hostname}`],
-	},
-	transform: {
-		cdn: (args) => {
-			args.origins = $resolve(args.origins).apply((val) => [...val, plausibleOrigin]);
+export const astro = new sst.aws.Astro("Astro", {
+  path: "apps/www",
+  dev: {
+    autostart: true,
+    command: "astro dev",
+  },
+  server: {
+    architecture: "arm64",
+    runtime: "nodejs22.x",
+  },
+  domain: {
+    name: hostname,
+    dns: sst.cloudflare.dns(),
+    redirects: [`www.${hostname}`],
+  },
+  transform: {
+    cdn: (args) => {
+      args.origins = $resolve(args.origins).apply((val) => [
+        ...val,
+        plausibleOrigin,
+      ]);
 
-			args.orderedCacheBehaviors = $resolve(args.orderedCacheBehaviors || []).apply((val) => [
-				...val,
-				analyticsScriptBehavior,
-				eventApiBehavior,
-			]);
-		},
-	},
+      args.orderedCacheBehaviors = $resolve(
+        args.orderedCacheBehaviors || [],
+      ).apply((val) => [...val, analyticsScriptBehavior, eventApiBehavior]);
+    },
+  },
 });
 
 export const outputs = {
-	www: astro.url,
+  www: astro.url,
 };
